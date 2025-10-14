ea0c54f41ce89701857345a123e47df90402e1d1
Nityam <nityamsoni26@gmail.com>
2025-10-14 11:29:16 +0530

    feat: add routing and components for user navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,36 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import {BrowserRouter ,Route,Routes } from 'react-router-dom';
+import Navbar from './Navbar.jsx';
+import Body from './Body.jsx';
+import Login from './Login.jsx';
+import Profile from './Profile.jsx';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      
- <div className="navbar bg-base-100 shadow-sm">
-  <div className="flex-1">
-    <a className="btn btn-ghost text-xl">💻👩‍❤️‍💋‍👨 DevTinder</a>
-  </div>
-  <div className="flex gap-2">
-    <div className="dropdown dropdown-end mx-20">
-      <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-        <div className="w-10 rounded-full">
-          <img
-            alt="Tailwind CSS Navbar component"
-            src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
-        </div>
-      </div>
-      <ul
-        tabIndex="-1"
-        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-        <li>
-          <a className="justify-between">
-            Profile
-            <span className="badge">New</span>
-          </a>
-        </li>
-        <li><a>Settings</a></li>
-        <li><a>Logout</a></li>
-      </ul>
-    </div>
-  </div>
-</div>
+
+    <BrowserRouter basename='/'>
+      <Routes>
+          {/* <Route path='/' element={<h1 className="text-3xl font-bold underline"> <Navbar /> </h1>} />
+
+        <Route path='/login' element={<h1 className="text-3xl font-bold underline">
+           <Navbar />
+          Login page</h1>} />
+         <Route path='/test' element={<h1 className="text-3xl font-bold underline">
+           <Navbar />
+          Test page</h1>} /> */}
+          <Route path='/' element={<Body />} >
+            <Route path='/login' element={<Login />} />
+            <Route path='/profile' element={<Profile />} />
+
+          </Route>
+
+      </Routes>
+
+      </BrowserRouter>
+      {/**/}
+ 
     </>
   )
 }
